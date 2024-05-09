694e63b24be75cc293a46fec2a9f4e0f6fbe9c72
Benja Aguilar <benjaaguilar02@gmail.com>
2024-05-09 11:24:46 -0300

    fix: ships use the avalible space
    now you cannot place a ship on another ship or a occupied space,
    ships on the edges only use the avalible space inside the board

--- src/gameboard.js
+++ src/gameboard.js
@@ -13,9 +13,15 @@ export default class Gameboard {
     this.destroyers = [new Ship(1, 'D0'), new Ship(1, 'D1')];
   }
 
-  placeShip(ship, y, x) {
+  placeShipX(ship, y, x) {
     if (y > 9 || x > 9) return;
     if (x + ship.length - 1 >= 10) return;
+
+    let hasSpace = true;
+    for (let i = 0; i < ship.length; i += 1) {
+      if (this.board[y][x + i] !== 'w') hasSpace = false;
+    }
+    if (!hasSpace) return;
 
     // push the ship id the number of times of his length and spread on splice
     const shipId = [];
@@ -26,12 +32,45 @@ export default class Gameboard {
     }
     this.board[y].splice(x, ship.length, ...shipId);
 
+    let xSubstract;
+    let addLength;
+    if (x === 0) {
+      // is on the left of the board
+      xSubstract = 0;
+      addLength = 1;
+      ocuppiedSpace.pop();
+    } else if (x + ship.length > 9) {
+      // is on the right of the board
+      xSubstract = 1;
+      addLength = 1;
+      ocuppiedSpace.pop();
+    } else {
+      xSubstract = 1;
+      addLength = 2;
+    }
+
     // takes the space on y of the ship (up and down)
-    this.board[y - 1].splice(x - 1, ship.length + 2, ...ocuppiedSpace);
-    this.board[y + 1].splice(x - 1, ship.length + 2, ...ocuppiedSpace);
+    if (y - 1 >= 0) {
+      this.board[y - 1].splice(
+        x - xSubstract,
+        ship.length + addLength,
+        ...ocuppiedSpace,
+      );
+    }
+    if (y + 1 <= 9) {
+      this.board[y + 1].splice(
+        x - xSubstract,
+        ship.length + addLength,
+        ...ocuppiedSpace,
+      );
+    }
 
     // takes the space on x of the ship (sides)
-    this.board[y][x - 1] = 'o';
-    this.board[y][x + ship.length] = 'o';
+    if (x - 1 >= 0) {
+      this.board[y][x - 1] = 'o';
+    }
+    if (x + ship.length <= 9) {
+      this.board[y][x + ship.length] = 'o';
+    }
   }
 }
