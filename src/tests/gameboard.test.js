b77b3d6b54005350cf067acf09860c8608006073
Benja Aguilar <benjaaguilar02@gmail.com>
2024-05-09 11:29:34 -0300

    test: place ships on ships or stick to each other

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -10,19 +10,18 @@ test('gameboard', () => {
 
 // can place ships
 test('ship placement 1', () => {
-  gameboard.placeShip(gameboard.aircraft, 2, 4);
-  console.log(gameboard.board);
+  gameboard.placeShipX(gameboard.aircraft, 2, 4);
   expect(gameboard.board[2][4]).toBe('A');
 });
 
 test('ship placement 2', () => {
-  gameboard.placeShip(gameboard.battleship, 9, 0);
+  gameboard.placeShipX(gameboard.battleship, 9, 0);
   expect(gameboard.board[9][0]).toBe('B');
 });
 
 // ships extends his length on x
 test('ship placement (extends ship length)', () => {
-  gameboard.placeShip(gameboard.aircraft, 2, 4);
+  gameboard.placeShipX(gameboard.aircraft, 2, 4);
   expect(gameboard.board[2][4]).toBe('A');
   expect(gameboard.board[2][5]).toBe('A');
   expect(gameboard.board[2][6]).toBe('A');
@@ -32,24 +31,38 @@ test('ship placement (extends ship length)', () => {
 
 // cannot place outside the board range
 test('ship placement (board range) 1', () => {
-  gameboard.placeShip(gameboard.crusier, 0, 10);
+  gameboard.placeShipX(gameboard.crusier, 0, 10);
   expect(gameboard.board[0][10]).toBe(undefined);
 });
 
 test('ship placement (board range) 2', () => {
-  gameboard.placeShip(gameboard.crusier, 0, 9);
+  gameboard.placeShipX(gameboard.crusier, 0, 9);
   expect(gameboard.board[0][9]).toBe('w');
   expect(gameboard.board[0][10]).toBe(undefined);
 });
 
 test('ship placement (board range) 3', () => {
-  gameboard.placeShip(gameboard.destroyers[0], 0, 9);
+  gameboard.placeShipX(gameboard.destroyers[0], 0, 9);
   expect(gameboard.board[0][9]).toBe('D0');
 });
 
+// cannot place a ship in to other ship
+test('ship placement (!in other ship)', () => {
+  gameboard.placeShipX(gameboard.submarines[1], 2, 4);
+  expect(gameboard.board[2][4]).toBe('A');
+  expect(gameboard.board[2][5]).toBe('A');
+});
+
 // cannot place ships stick to others
-test('ship placement (!sticky)', () => {
-  gameboard.placeShip(gameboard.submarines[0], 1, 5);
+test('ship placement (!sticky) 1', () => {
+  gameboard.placeShipX(gameboard.submarines[0], 1, 5);
   expect(gameboard.board[1][5]).toBe('o');
   expect(gameboard.board[1][6]).toBe('o');
+});
+
+test('ship placement (!sticky) 2', () => {
+  gameboard.placeShipX(gameboard.submarines[0], 1, 2);
+  console.log(gameboard.board);
+  expect(gameboard.board[1][2]).toBe('w');
+  expect(gameboard.board[1][3]).toBe('o');
 });
